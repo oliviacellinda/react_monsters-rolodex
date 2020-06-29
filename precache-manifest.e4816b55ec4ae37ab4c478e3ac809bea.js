1134369880941e6df85f0579b35a6f9032709612
oliviacellinda <oliviacellinda@gmail.com>
2020-06-29 14:48:37 +0700

    Updates

---
+++ precache-manifest.e4816b55ec4ae37ab4c478e3ac809bea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1573b8ff154a99021ece960998820c6",
+    "url": "/react_monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d21f316892fd5fbf7641",
+    "url": "/react_monsters-rolodex/static/css/main.b52215df.chunk.css"
+  },
+  {
+    "revision": "5a474fdd99a7182bc7a6",
+    "url": "/react_monsters-rolodex/static/js/2.4b2d8aa6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react_monsters-rolodex/static/js/2.4b2d8aa6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d21f316892fd5fbf7641",
+    "url": "/react_monsters-rolodex/static/js/main.1255e9db.chunk.js"
+  },
+  {
+    "revision": "8dc437a2310e6b03288f",
+    "url": "/react_monsters-rolodex/static/js/runtime-main.1787a88c.js"
+  }
+]);
